80087ddc2fb5a8a18dff5de6eeaf83f17009d0b8
MackInSpace <amackey@lssu.edu>
2025-03-05 21:39:37 -0700

    created basic website setup with icons, done for the day

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,16 @@ export default function Layout(props) {
     const { children } = props
 
     const header = (
-        <header></header>
+        <header>
+            <div>
+                <h1 className="text-gradient">X-CAFFEINE</h1>
+                <p>For Coffee Enthusiasts</p>
+            </div>
+            <button>
+                <p>Sign up free</p>
+                <i class="fa-solid fa-mug-hot"></i>
+            </button>
+        </header>
     )
 
     const footer = (
