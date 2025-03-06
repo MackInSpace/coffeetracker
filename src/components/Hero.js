fd15fd6ec3a16aa56c8c288aa4de1fc39ac8772f
MackInSpace <amackey@lssu.edu>
2025-03-05 20:57:03 -0700

    moved coffeetracker from jsx to js format

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+export default function Hero() {
+    return (
+        <>
+            <h1>Coffee Tracking for Coffee <abbr title="An enthusiast or devotee">Addicts</abbr>!</h1>
+            <div className="benefits-list">
+                <h3 className="font-bolder">Try <span>Caffiend</span> and start ...</h3>
+                <p>✅ Tracking every coffee</p>
+                <p>✅ Measuring your blood caffeine levels</p>
+                <p>✅ Costing and quanitifying your addition</p>
+            </div>
+        </>
+    )
+}
